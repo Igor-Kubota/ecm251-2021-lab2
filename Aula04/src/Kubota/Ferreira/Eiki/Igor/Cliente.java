7dc704dc4c190b2f402f7681e29f6b89c474bc5e
Igor Kubota <igor-eiki@hotmail.com>
2021-03-11 09:44:11 -0300

    Intro to Get/Set

---
+++ Aula04/src/Kubota/Ferreira/Eiki/Igor/Cliente.java
@@ -0,0 +1,15 @@
+package Kubota.Ferreira.Eiki.Igor;
+
+public class Cliente {
+    String Titular;
+    String CPF;
+
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "Titular='" + Titular + '\'' +
+                ", CPF='" + CPF + '\'' +
+                '}';
+    }
+}
