443e8d0e4d9525462cbb77fc0c9d78795584b4cc
Igor Kubota <igor-eiki@hotmail.com>
2021-03-15 10:15:21 -0300

    Criação do Construtor do Sistema

--- Aula05/src/Kubota/Ferreira/Eiki/Igor/Sistema.java
+++ Aula05/src/Kubota/Ferreira/Eiki/Igor/Sistema.java
@@ -1,8 +1,38 @@
 package Kubota.Ferreira.Eiki.Igor;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private boolean executarsistema;
+    private Scanner scanner;
+
     public void executar(){
-        System.out.println("Aqui!");
+        int opcao;
+        while(executarsistema) {
+            exibirmenu();
+            opcao = scanner.nextInt();
+
+
+
+        }
+    }
+
+
+    public Sistema(){
+        this.executarsistema = true;
+        this.scanner = new Scanner(System.in);
+    }
+
+    private void exibirmenu() {
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar Dinheiro");
+        System.out.println("4 - Transferir Dinheiro");
+        System.out.println("5 - Pagar conta(Titulo)");
+        System.out.println("0 - Encerrar sistema");
+
+
     }
 }
 
