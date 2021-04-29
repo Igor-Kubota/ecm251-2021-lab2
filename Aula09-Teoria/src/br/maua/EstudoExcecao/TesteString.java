d14e2bfe81d3839346c784a0643b656db0128e3f
Igor Kubota <igor-eiki@hotmail.com>
2021-04-29 08:46:33 -0300

    try-catch

--- Aula09-Teoria/src/br/maua/EstudoExcecao/TesteString.java
+++ Aula09-Teoria/src/br/maua/EstudoExcecao/TesteString.java
@@ -6,7 +6,16 @@ public class TesteString {
     public static void main(String[] args) {
         String frase = null;
         String novaFrase = null;
-        novaFrase = frase.toUpperCase();
+        try {
+            novaFrase = frase.toUpperCase();
+        }
+        catch (NullPointerException exception){
+            System.out.println("Uma Excecao ocorreu: "+ exception.getMessage());
+            frase = "";
+            novaFrase = "";
+
+        }
+
         System.out.println("Frase Antiga:" + frase);
         System.out.println("Frase Nova:" + novaFrase);
     }
