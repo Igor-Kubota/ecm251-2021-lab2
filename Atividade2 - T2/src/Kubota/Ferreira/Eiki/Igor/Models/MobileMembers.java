d2ec5339d04a5457cbe4d507a9508bb96415f3be
Igor Kubota <igor-eiki@hotmail.com>
2021-06-25 18:46:36 -0300

    Implementação inicial da subclasse MobileMembers.java

--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Models/MobileMembers.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Models/MobileMembers.java
@@ -1,5 +1,22 @@
 package Kubota.Ferreira.Eiki.Igor.Models;
 
+import Kubota.Ferreira.Eiki.Igor.Enums.TiposMembros;
+
 public class MobileMembers extends Membro{
 
+    public MobileMembers(String nome, String email, TiposMembros funcao){
+        super(nome, email, funcao);
+
+    }
+
+
+    @Override
+    public void ExibirRelatorio() {
+
+    }
+
+    @Override
+    public void Mensagem() {
+
+    }
 }
