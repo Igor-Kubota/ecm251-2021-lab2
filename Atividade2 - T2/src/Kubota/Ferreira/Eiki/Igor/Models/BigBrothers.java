07d70a2b0356452d07d8379a5b7024d9ab4751ce
Igor Kubota <igor-eiki@hotmail.com>
2021-06-25 19:01:36 -0300

    Implementação inicial da Subclasse BigBrothers.java

--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Models/BigBrothers.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Models/BigBrothers.java
@@ -1,5 +1,24 @@
 package Kubota.Ferreira.Eiki.Igor.Models;
 
+import Kubota.Ferreira.Eiki.Igor.Enums.TiposMembros;
+
 public class BigBrothers extends Membro{
-    
+
+    public BigBrothers(String nome, String email, TiposMembros funcao){
+        super(nome, email, funcao);
+
+    }
+
+
+    @Override
+    public void ExibirRelatorio() {
+
+    }
+
+    @Override
+    public void Mensagem() {
+
+    }
+}
+
 }
