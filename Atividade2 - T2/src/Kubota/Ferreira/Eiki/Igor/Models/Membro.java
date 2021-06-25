611302e787759bf22f33523b22f2683f8d10dea5
Igor Kubota <igor-eiki@hotmail.com>
2021-06-25 17:34:51 -0300

    Create Classe mãe Membro.java

---
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Models/Membro.java
@@ -0,0 +1,9 @@
+package Kubota.Ferreira.Eiki.Igor.Models;
+
+import Kubota.Ferreira.Eiki.Igor.Interfaces.IApresentacao;
+import Kubota.Ferreira.Eiki.Igor.Interfaces.IPostarMensagem;
+
+public abstract class Membro implements IApresentacao, IPostarMensagem {
+    
+
+}
