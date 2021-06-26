ef1c4925839a1242bfb0d56c8d3dc8c3293f2f7f
Igor Kubota <igor-eiki@hotmail.com>
2021-06-25 22:23:36 -0300

    Update do Menu em Sistema.java

--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Controle/Sistema.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Controle/Sistema.java
@@ -3,12 +3,13 @@ package Kubota.Ferreira.Eiki.Igor.Controle;
 import Kubota.Ferreira.Eiki.Igor.Enums.Horarios;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-public class Sistema {
+public class Sistema{
     final private static Scanner scanner = new Scanner(System.in);
     private static Horarios HoraAtual = Horarios.REGULAR;
 
-    public static void run() {
+    public static void run()throws InterruptedException  {
         boolean running = true;
 
 
@@ -18,19 +19,30 @@ public class Sistema {
 
                 "--------MENU DE OPÇÕES--------\n" +
                 "HORARIO ATUAL: " + HoraAtual + "\n" +
-                "1 - Cadastrar um novo Membro:\n"+
-                "2 - Postar mensagem\n" +
-                "3 - Trocar o horário de trabalho\n" +
-                "4 - Encerrar o Sistema\n"+
+                "1 - Cadastrar um novo Membro.\n"+
+                "2 - Postar mensagem.\n" +
+                "3 - Exibir Relatório de Informações.\n" +
+                "4 - Remover um Membro.\n" +
+                "5 - Trocar o horário de trabalho.\n" +
+                "6 - Encerrar o Sistema.\n"+
                 "Digite a sua Opção: "
         );
         int input = scanner.nextInt();
         switch (input){
             case 1 :
                 System.out.println("bom dia\n");
+                TimeUnit.SECONDS.sleep(1);
+                break;
+
+            case 2:
+                System.out.println("b");
                 break;
 
             case 3:
+                System.out.println("A");
+                break;
+
+            case 4:
                 if(HoraAtual == Horarios.REGULAR) {
                     HoraAtual = Horarios.EXTRA;
                 }
@@ -39,7 +51,7 @@ public class Sistema {
                 }
                 break;
 
-            case 4:
+            case 5:
                 System.out.println("Encerrando Sistema");
                 running = false;
                 break;
--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Main.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Main.java
@@ -4,7 +4,7 @@ import Kubota.Ferreira.Eiki.Igor.Controle.Sistema;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         Sistema sistema = new Sistema();
         sistema.run();
     }
