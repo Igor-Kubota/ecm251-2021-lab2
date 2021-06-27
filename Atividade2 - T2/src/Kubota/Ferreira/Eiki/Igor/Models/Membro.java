905dc3e933ac4298d87db0fc3a8d32769c42c019
Igor Kubota <igor-eiki@hotmail.com>
2021-06-27 01:41:10 -0300

    Add JavaDocs to Membro.java

--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Models/Membro.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Models/Membro.java
@@ -8,43 +8,79 @@ import Kubota.Ferreira.Eiki.Igor.Enums.TiposMembros;
 
 public abstract class Membro implements IApresentacao, IPostarMensagem {
 
-    //atributos
+    /**
+     * Atributos da Classe
+     */
     private String nome;
     private String email;
     private TiposMembros funcao;
 
-    //Construtor
+    /**
+     * Construtor da classe membro
+     * @param nome nome do membro
+     * @param email email do membro
+     * @param funcao funcao do membro
+     */
     public Membro(String nome, String email, TiposMembros funcao) {
         this.nome = nome;
         this.email = email;
         this.funcao = funcao;
     }
 
-    //getter e setter do nome
+    /**
+     * Getter do nome
+     * @return retorna o nome de um mebro
+     */
     public String getNome() {
         return nome;
     }
+
+    /**
+     * Setter do nome
+     * @param nome nome atribuido a um membro
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    //getter e setter do email
+
+    /**
+     * Getter do email
+     * @return retorna o email de um membro
+     */
     public String getEmail() {
         return email;
     }
+
+    /**
+     *Setter do email
+     * @param email email atribuido a um membro
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    //getter e setter da funcao
+
+    /**
+     * Getter da funcao
+     * @return retorna a função de um membro
+     */
     public TiposMembros getFuncao() {
         return funcao;
     }
+
+    /**
+     * Setter da funcao
+     * @param funcao funcao atribuida a um determinado membro
+     */
     public void setFuncao(TiposMembros funcao) {
         this.funcao = funcao;
     }
 
-    //tostring
+    /**
+     * ToString do membro
+     * @return Estado atual do membro
+     */
     @Override
     public String toString() {
         return "Membro{" + "nome='" + nome + '\'' + ", email='" + email + '\'' + ", funcao=" + funcao + '}';
