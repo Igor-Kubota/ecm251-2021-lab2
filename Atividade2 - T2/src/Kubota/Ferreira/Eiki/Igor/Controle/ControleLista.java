461d84e0c262c75fa260002a909c363d6d925f50
Igor Kubota <igor-eiki@hotmail.com>
2021-06-27 01:33:39 -0300

    Add Javadocs de ControleLista.java

--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Controle/ControleLista.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Controle/ControleLista.java
@@ -7,22 +7,42 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 
+/**
+ * Classe responsável pelo controle da lista
+ */
 public class ControleLista {
     private ArrayList<Membro> ListaDeMembros = new ArrayList<Membro>();
 
-    //getter
+    /**
+     * Getter da lista de membros
+     * @return retorna a lista de membros
+     */
     public ArrayList<Membro> getListaDeMembros() {
         return ListaDeMembros;
     }
 
+
+    /**
+     * Metodo responsavel pela adição de membros na lista
+     * @param membro Novo objeto da classe membro a ser adicionado
+     */
     public void AddMembro(Membro membro){
         this.ListaDeMembros.add(membro);
     }
 
+    /**
+     * Metodo responsavel pela remoção de membros na lista
+     * @param membro Objeto da classe membro a ser removido
+     */
     public void RemoverMembro(Membro membro){
         this.ListaDeMembros.remove(membro);
 
     }
+
+    /**
+     * Método responsável pela limpeza da lista
+     * @throws IOException Retorna uma exceção do tipo IO
+     */
     public static void Limpa()throws IOException {
         Writer out = new FileWriter("Dados.txt");
 
