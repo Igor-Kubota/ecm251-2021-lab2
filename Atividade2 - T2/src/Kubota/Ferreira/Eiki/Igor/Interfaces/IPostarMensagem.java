895a13c4d88fe49f64243a54994d8951d7d65529
Igor Kubota <igor-eiki@hotmail.com>
2021-06-27 02:21:39 -0300

    Updade JavaDoc in IPostarMensagem.java e docs Folder

--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Interfaces/IPostarMensagem.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Interfaces/IPostarMensagem.java
@@ -6,6 +6,7 @@ public interface IPostarMensagem {
 
     /**
      * Interface responsável pela postagem da mensagem através da void Mensagem
+     * @param horario horario em que a mensagem sera postada
      */
 
     public abstract void Mensagem(Horarios horario);
