5eb610d9a51fa215100c842f26aa3f665dd535a4
Igor Kubota <igor-eiki@hotmail.com>
2021-06-27 00:59:00 -0300

    Add Javadocs das Interfaces

--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Interfaces/IApresentacao.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Interfaces/IApresentacao.java
@@ -1,5 +1,8 @@
 package Kubota.Ferreira.Eiki.Igor.Interfaces;
 
 public interface IApresentacao {
+    /**
+     * Interface responsável pela apresentação através da void ExibirRelatorio
+     */
     public abstract void ExibirRelatorio();
 }
--- Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Interfaces/IPostarMensagem.java
+++ Atividade2 - T2/src/Kubota/Ferreira/Eiki/Igor/Interfaces/IPostarMensagem.java
@@ -3,5 +3,10 @@ package Kubota.Ferreira.Eiki.Igor.Interfaces;
 import Kubota.Ferreira.Eiki.Igor.Enums.Horarios;
 
 public interface IPostarMensagem {
+
+    /**
+     * Interface responsável pela postagem da mensagem através da void Mensagem
+     */
+
     public abstract void Mensagem(Horarios horario);
 }
